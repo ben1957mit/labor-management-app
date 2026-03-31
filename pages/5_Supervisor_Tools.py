3151bc3d7cccae985c135d876f7bb9cb429c38ed
ben1957mit <ben1957mit@gmail.com>
2026-03-30 21:08:33 -0500

    Add Supervisor Tools page with initial content

---
+++ pages/5_Supervisor_Tools.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("🛠 Supervisor Tools")
+
+st.write("This page can host additional calculators and tools for supervisors.")
+st.info("Future ideas: staffing scenarios, what-if analysis, labor cost projections, etc.")
